0f59f0a99f4ed7e163d0ffcbed9cc6ba97782f75
mythlesh <mythlesh114@gmail.com>
2026-08-19 21:45:04 +0545

    fixed contact issue

--- api/contact.js
+++ api/contact.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
 
       // CHANGE THIS TO THE EMAIL WHERE YOU WANT
       // PORTFOLIO MESSAGES TO ARRIVE.
-      to: ["mythleshyadav114@gmail.com"],
+      to: ["mythlesh114@gmail.com"],
 
       replyTo: email,
 
